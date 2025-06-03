3268ab599f81b157fd061e3ffdc384bef6b0eec4
IsabelleCarvalhoCruz <isabelle.cruz@sptech.school>
2025-06-03 15:18:20 -0300

    Fiz alterações na dashboard para ir de acordo com o quizz e adicionei validações no site

--- Pesquisa e Inovação/Trabalho Individual/src/database/script-tabelas.sql
+++ Pesquisa e Inovação/Trabalho Individual/src/database/script-tabelas.sql
@@ -1,69 +1,8 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
-
-/*
-comandos para mysql server
-*/
-
--- CREATE DATABASE aquatech;
-
--- USE aquatech;
-
--- CREATE TABLE empresa (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	razao_social VARCHAR(50),
--- 	cnpj CHAR(14),
--- 	codigo_ativacao VARCHAR(50)
--- );
-
--- CREATE TABLE usuario (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	nome VARCHAR(50),
--- 	email VARCHAR(50),
--- 	senha VARCHAR(50),
--- 	fk_empresa INT,
--- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
--- );
-
--- CREATE TABLE aviso (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	titulo VARCHAR(100),
--- 	descricao VARCHAR(150),
--- 	fk_usuario INT,
--- 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
--- );
-
--- create table aquario (
--- /* em nossa regra de negócio, um aquario tem apenas um sensor */
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	descricao VARCHAR(300),
--- 	fk_empresa INT,
--- 	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
--- );
-
--- /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
--- create table medida (
--- 	id INT PRIMARY KEY AUTO_INCREMENT,
--- 	dht11_umidade DECIMAL,
--- 	dht11_temperatura DECIMAL,
--- 	luminosidade DECIMAL,
--- 	lm35_temperatura DECIMAL,
--- 	chave TINYINT,
--- 	momento DATETIME,
--- 	fk_aquario INT,
--- 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
--- );
-
--- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
--- insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
--- insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
--- insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
-
+-- CRIANDO O BANCO DE DADOS PARA O PROJETO
 CREATE DATABASE Fonteyn;
 USE Fonteyn;
 
+-- CRIANDO AS TABELAS
 CREATE TABLE Usuario(
 idUsuario int primary key auto_increment,
 nome varchar(45),
@@ -74,48 +13,37 @@ senha varchar(45)
 CREATE TABLE Forum(
 idForum int primary key auto_increment,
 titulo varchar(45),
-descrição varchar(45),
-mensagem varchar(45),
+descrição varchar(200),
 fkUsuario int,
 constraint fkUsuario foreign key (fkUsuario) 
+references Usuario(idUsuario)
+);
+
+CREATE TABLE Blog(
+idBlog int primary key auto_increment,
+titulo varchar(45),
+descrição varchar(200),
+fkUsuarioBlog int,
+constraint fkUsuarioBlog foreign key (fkUsuarioBlog)
 references Usuario(idUsuario)
 );
 
 CREATE TABLE Curso(
 idCurso int primary key auto_increment,
 nome varchar(45),
-descrição varchar(45),
-notaTotal int,
+descrição varchar(200),
 fkUsuarioCurso int,
+Status varchar(45),
+constraint chkStatus check (Status in ('Feito', 'Não Feito')),
 constraint fkUsuarioCurso foreign key (fkUsuarioCurso) 
 references Usuario(idUsuario)
-);
-
-CREATE TABLE Quizz(
-idQuizz int primary key auto_increment,
-nome varchar(45),
-descrição varchar(45),
-gabarito varchar(45)
-);
-
-CREATE TABLE Pontuação(
-idPontuação int,
-Total int,
-fkUsuarioPontuação int,
-fkQuizz int,
-constraint fkUsuarioPontuação foreign key (fkUsuarioPontuação) 
-references Usuario(idUsuario),
-constraint fkQuizz foreign key (fkQuizz) 
-references Quizz(idQuizz),
-constraint pkPontQuizzUsuario primary key (idPontuação, fkUsuarioPontuação, fkQuizz)
 );
 
 CREATE TABLE Exercicio(
 idExercicio int,
 fkCurso int,
 nome varchar(45),
-descrição varchar(45),
-notaFinal int,
+descrição varchar(200),
 constraint fkCurso foreign key (fkCurso)
 references Curso(idCurso),
 constraint pkCursoExercicio primary key (idExercicio, fkCurso)
@@ -124,8 +52,10 @@ constraint pkCursoExercicio primary key (idExercicio, fkCurso)
 CREATE TABLE Desempenho(
 idDesempenho int,
 NotaMedia int,
-Status varchar(45),
-constraint chkStatus check (Status in ('Feito', 'Não Feito', 'Fazendo')),
+NotaUsuario int,
+Acertos int,
+Erros int,
+Porcentagem int,
 fkCursoDesempenho int,
 fkCursoUsuario int, 
 fkExercicio int, 
@@ -142,6 +72,21 @@ constraint pkDesempenho primary key (idDesempenho,fkCursoDesempenho,
 fkCursoUsuario,fkExercicio,fkCursoExercicio)
 );
 
+-- DANDO SELECT PARA VERIFICAR SE OS DADOS DO LOGIN E CADASTROO ESTÃO SENDO INSERIDOS
+select * from Usuario;
 
+-- INSERINDO DADOS 
+insert into Curso (nome, descrição) values
+('Diferentes técnicas de ballet','Já pensou em ser bailarina mas não sabe por onde começar? Aprenda um pouco mais sobre aqui!'),
+('Alongamento e Fortalecimento', 'No mundo do ballet ter sua força e elasticidade em dia é muito importante para execução de passos mais complexos, saiba como manter sua saúde em dia!');
+
+insert into Exercicio (idExercicio, nome, descrição, fkCurso) values
+(1, 'Diferentes técnicas de ballet','Exercicios para você aprofundar seus conhecimentos sobre a técnica do ballet!', 1),
+(2, 'Alongamento e Fortalecimento','Exercicios para você manter sua saúde em dia!', 2);
+
+insert into Blog(titulo,descrição, fkUsuarioBlog) values
+('A História do Ballet','Saiba um pouco mais sobre o surgimento do ballet!', 1),
+('Metodologias do ballet','Saiba diferenciar as metodologias de ballet, onde surgiram e sua importância!', 1),
+('Variações de Repertório', 'Saiba um pouco mais sobre as diferentes encenações e variações criadas no mundo do ballet!', 1);
 
 
