65cc65f384765898c621610c1dce1b5ec658a480
IsabelleCarvalhoCruz <isabelle.cruz@sptech.school>
2026-05-06 15:16:59 -0300

    Merge branch 'main' of https://github.com/IsabelleCarvalhoCruz/Trabalho-Individual

--- Pesquisa e Inovação/Trabalho Individual/auto-commit.sh
+++ Pesquisa e Inovação/Trabalho Individual/auto-commit.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+echo "Adicionando arquivos..."
+git add .
+
 if [[ -z $(git status --porcelain) ]]; then
   echo "Nada para commitar"
-  exit 0
+else
+  echo "Criando commit..."
+  git commit -m "${1:-auto commit}"
 fi
 
-git add .
-git commit -m "${1:-auto commit}"
+echo "Enviando para o GitHub..."
 git push origin main
+
+echo "✅ Finalizado"
